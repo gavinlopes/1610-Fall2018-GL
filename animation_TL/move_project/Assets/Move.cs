c7bc4c3c6897d4dbde9b15958fec06481a262391
Gavin Lopes <gavin.l.lopes@gmail.com>
2018-09-12 15:34:30 -0600

    move project for September-12

---
+++ animation_TL/move_project/Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public CharacterController Character;
+	// Update is called once per frame
+	public float Speed;
+	public Vector3 MoveSpeed;
+	public Vector3 RotateSpeed;
+	
+	void Update ()
+	{
+
+		MoveSpeed.x += Speed*Input.GetAxis("Horizontal");
+		MoveSpeed *= Time.deltaTime;
+
+		Character.Move(MoveSpeed);
+
+	}
+}
