6b283f925a4457d90d53735d99df36a95cdc176d
Gavin Lopes <gavin.l.lopes@gmail.com>
2018-09-26 16:08:44 -0600

    update 9/36

---
+++ GL_practice/functions_hw/hw_functions/Assets/MovePractice.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePractice : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+
+    public CharacterController Character;
+
+    public float Speed;
+    public Vector3 MoveSpeed;
+    public Vector3 RotateSpeed;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        MoveSpeed.x += Speed * Input.GetAxis("Horizonatal");
+        MoveSpeed *= Time.deltaTime;
+
+        Character.Move(MoveSpeed);
+		
+	}
+}
