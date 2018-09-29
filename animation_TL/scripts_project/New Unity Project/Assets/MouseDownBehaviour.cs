351a6f618df896b82302698def31cc4b7cbe67a4
Gavin Lopes <gavin.l.lopes@gmail.com>
2018-09-29 17:34:30 -0600

    scripts_hw_9-29

---
+++ animation_TL/scripts_project/New Unity Project/Assets/MouseDownBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class MouseDownBehaviour : MonoBehaviour 
+{
+	public UnityEvent Event;
+
+	private void OnMouseDown()
+	{
+		Event.Invoke();
+	}	
+}
