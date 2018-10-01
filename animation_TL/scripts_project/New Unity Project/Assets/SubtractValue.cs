c0579a8d27108275d23a76af19b9d9fb52845057
Gavin Lopes <gavin.l.lopes@gmail.com>
2018-10-01 11:59:50 -0600

    SubtractValue

---
+++ animation_TL/scripts_project/New Unity Project/Assets/SubtractValue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class SubtractValue : ScriptableObject
+{
+	public FloatData ValueObj;
+	public UnityEvent Event;
+
+	public void SubtractValuetoObj (FloatData data)
+	{
+		ValueObj.Value -= data.Value;
+	}
+}
