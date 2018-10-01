50a9af1489aaa4db4cce50d70833924b1ba9155e
Gavin Lopes <gavin.l.lopes@gmail.com>
2018-10-01 12:10:07 -0600

    divide value

---
+++ animation_TL/scripts_project/New Unity Project/Assets/DivideValue.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DivideValue : ScriptableObject 
+{
+	public FloatData ValueObj;
+	public UnityEvent Event;
+
+	public void DivideValuetoObj (FloatData data)
+	{
+		ValueObj.Value /= data.Value;
+	}
+}
