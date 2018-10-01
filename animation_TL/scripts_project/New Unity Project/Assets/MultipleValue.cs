2716e906d7808664b0727810bfbdb3add8e40942
Gavin Lopes <gavin.l.lopes@gmail.com>
2018-10-01 12:46:03 -0600

    times value

---
+++ animation_TL/scripts_project/New Unity Project/Assets/MultipleValue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class MultipleValue : ScriptableObject 
+{
+	public FloatData ValueObj;
+	public UnityEvent Event;
+
+	public void MultipleValuetoObj (FloatData data)
+	{
+		ValueObj.Value x= data.Value;
+	}
+}
