a701928c9b79e53146da6343942030fe572b8a72
Gavin Lopes <gavin.l.lopes@gmail.com>
2018-10-08 08:27:38 -0600

    if statements

---
+++ animation_TL/scripts_project/New Unity Project/Assets/IfStatements.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour 
+{
+
+	public bool LightOn;
+	public string Password;
+	public int A, B, C;
+
+	public FloatData WizardPower, PawnPower;
+
+	void Update () {
+
+		if (WizardPower.Value > PawnPower.Value)
+		{
+			print("Wizard wins");
+		}
+
+		if (Lighton)
+		{
+			print("The light is on.");
+		}
+		else
+		{
+			print("The light is off.");
+		}
+
+		if (Password != "OU812")
+		{
+			print("You are correct!");
+		}
+
+		if (C == A + B)
+		{
+			print(C);
+		}
+	}
+}
