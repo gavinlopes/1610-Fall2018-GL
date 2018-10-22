8560c475dc7c5e0da0f41bb26950601592c520d8
Gavin Lopes <gavin.l.lopes@gmail.com>
2018-10-21 23:01:45 -0600

    Health
    10/17 missed class

---
+++ animation_TL/scripts_project/New Unity Project/Assets/ValueChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ValueChange : ScriptableObject
+{
+	public FloatData Data;
+
+	public void AddValue (FloatData obj)
+	{
+		Data.Value += obj.Value;
+	}
+
+	public void SubtractValue (FloatData obj)
+	{
+		Data.Value -= obj.Value;
+	}
+}
