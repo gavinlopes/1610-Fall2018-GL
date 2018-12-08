695f84948f1706575db8a2965490fe7f8fc2b74f
Gavin Lopes <gavin.l.lopes@gmail.com>
2018-12-07 18:21:48 -0700

    game_update

---
+++ animation_TL/scripts_project/New Unity Project/Assets/Final_Game_Scripts/TakeDamage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TakeDamage : MonoBehaviour
+{
+
+	public UnityEvent Event;
+
+	void OnTriggerEnter(Collider other)
+	{
+		other.gameObject.GetComponent<UpdateImageValue>();
+	}
+	
+		
+}
